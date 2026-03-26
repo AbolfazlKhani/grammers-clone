AbolfazlKhani/grammers-clone

commit 1b5785710a82bfad3a3a8e6bc22ba12598d036cc

Fix first channel update was not returned
Closes #26.

grammers-session/src/message_box/mod.rs
@@ -172,12 +172,12 @@ impl MessageBoxes {
     fn update_or_insert_entry(
         &mut self,
         key: Key,
-        insert: LiveEntry,
+        inserter: impl FnOnce() -> LiveEntry,
         updater: impl FnOnce(&mut LiveEntry),
     ) {
         match self.entries.binary_search_by_key(&key, |entry| entry.key) {
             Ok(i) => updater(&mut self.entries[i]),
-            Err(i) => self.entries.insert(i, insert),
+            Err(i) => self.entries.insert(i, inserter()),
         }
     }
 
@@ -484,13 +484,18 @@ impl MessageBoxes {
             }
 
             let mut gap_deadline = None;
+            let mut return_new_update = false;
+            let mut return_changed_update = false;
             self.update_or_insert_entry(
                 info.key,
-                LiveEntry {
-                    key: info.key,
-                    pts: info.pts,
-                    deadline,
-                    possible_gap: None,
+                || {
+                    return_new_update = true;
+                    LiveEntry {
+                        key: info.key,
+                        pts: info.pts,
+                        deadline,
+                        possible_gap: None,
+                    }
                 },
                 |entry| {
                     match (entry.pts + info.count).cmp(&info.pts) {
@@ -498,7 +503,7 @@ impl MessageBoxes {
                         Ordering::Equal => {
                             entry.pts = info.pts;
                             entry.deadline = deadline;
-                            result.push((update, mk_state(Some(info.into()))));
+                            return_changed_update = true;
                         }
                         // Ignore
                         Ordering::Greater => {
@@ -521,7 +526,7 @@ impl MessageBoxes {
                                     updates: Vec::new(),
                                 })
                                 .updates
-                                .push(update);
+                                .push(update.clone());
                         }
                     };
 
@@ -553,6 +558,9 @@ impl MessageBoxes {
                     }
                 },
             );
+            if return_new_update || return_changed_update {
+                result.push((update, mk_state(Some(info.into()))));
+            }
 
             self.next_deadline = self.next_deadline.min(gap_deadline.unwrap_or(deadline));
         }

grammers-session/src/message_box/tests.rs
@@ -78,6 +78,25 @@ fn updates(date: i32, seq: i32, pts: i32) -> UpdatesLike {
     }))
 }
 
+fn channel_update(channel_id: i64, pts: i32) -> tl::enums::Update {
+    tl::enums::Update::DeleteChannelMessages(tl::types::UpdateDeleteChannelMessages {
+        channel_id,
+        messages: Vec::new(),
+        pts,
+        pts_count: 1,
+    })
+}
+
+fn channel_updates(channel_id: i64, date: i32, seq: i32, pts: i32) -> UpdatesLike {
+    UpdatesLike::Updates(tl::enums::Updates::Updates(tl::types::Updates {
+        updates: vec![channel_update(channel_id, pts)],
+        users: Vec::new(),
+        chats: Vec::new(),
+        date,
+        seq,
+    }))
+}
+
 fn updates_ok(date: i32, seq: i32, pts: i32) -> Result<UpdateAndPeers, Gap> {
     Ok((
         vec![(
@@ -586,4 +605,27 @@ fn test_process_socket_updates_trickle_causes_gap() {
     advance_time_by(2 * (POSSIBLE_GAP_TIMEOUT / 5));
     assert_eq!(message_boxes.check_deadlines(), gap_deadline);
     assert!(message_boxes.get_difference().is_some());
+}
+
+#[test]
+fn test_process_socket_update_for_new_channel_dispatches_first_update() {
+    reset_time();
+    let mut message_boxes = MessageBoxes::new();
+    let (channel_id, date, seq, pts) = (12, 0, 0, 78);
+    let ret = message_boxes.process_updates(channel_updates(channel_id, date, seq, pts));
+    assert_eq!(
+        ret,
+        Ok((
+            vec![(
+                channel_update(channel_id, pts),
+                State {
+                    date,
+                    seq,
+                    message_box: Some(MessageBox::Channel { channel_id, pts })
+                }
+            )],
+            vec![],
+            vec![]
+        ))
+    );
 }

commit c504e2731a0767230d6b7945d9cbf953f933a8bb

Fix regression on returned update order on first gap
Introduced by 1b5785710a82bfad3a3a8e6bc22ba12598d036cc.

grammers-session/src/message_box/mod.rs
@@ -169,15 +169,16 @@ impl MessageBoxes {
         }
     }
 
-    fn update_or_insert_entry(
-        &mut self,
-        key: Key,
-        inserter: impl FnOnce() -> LiveEntry,
-        updater: impl FnOnce(&mut LiveEntry),
-    ) {
-        match self.entries.binary_search_by_key(&key, |entry| entry.key) {
+    fn force_update_entry(&mut self, mut entry: LiveEntry, updater: impl FnOnce(&mut LiveEntry)) {
+        match self
+            .entries
+            .binary_search_by_key(&entry.key, |entry| entry.key)
+        {
             Ok(i) => updater(&mut self.entries[i]),
-            Err(i) => self.entries.insert(i, inserter()),
+            Err(i) => {
+                updater(&mut entry);
+                self.entries.insert(i, entry);
+            }
         }
     }
 
@@ -484,18 +485,12 @@ impl MessageBoxes {
             }
 
             let mut gap_deadline = None;
-            let mut return_new_update = false;
-            let mut return_changed_update = false;
-            self.update_or_insert_entry(
-                info.key,
-                || {
-                    return_new_update = true;
-                    LiveEntry {
-                        key: info.key,
-                        pts: info.pts,
-                        deadline,
-                        possible_gap: None,
-                    }
+            self.force_update_entry(
+                LiveEntry {
+                    key: info.key,
+                    pts: info.pts - info.count,
+                    deadline,
+                    possible_gap: None,
                 },
                 |entry| {
                     match (entry.pts + info.count).cmp(&info.pts) {
@@ -503,7 +498,7 @@ impl MessageBoxes {
                         Ordering::Equal => {
                             entry.pts = info.pts;
                             entry.deadline = deadline;
-                            return_changed_update = true;
+                            result.push((update, mk_state(Some(info.into()))));
                         }
                         // Ignore
                         Ordering::Greater => {
@@ -558,9 +553,6 @@ impl MessageBoxes {
                     }
                 },
             );
-            if return_new_update || return_changed_update {
-                result.push((update, mk_state(Some(info.into()))));
-            }
 
             self.next_deadline = self.next_deadline.min(gap_deadline.unwrap_or(deadline));
         }